4a0a27019fd4c4db643af02de5b99549f69a22d6
Koyena-Mitra <mitrakoyna2020@gmail.com>
2025-09-02 16:32:57 +0530

    WEBSOCKETING
    taking reference from apna college

---
+++ server/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import { WebSocketServer } from "ws";
+
+const app = express();
+const port = 8080;
+
+const server = app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
+const wss = new WebSocketServer({server});
+wss.on("connection", (ws) => {
+    
+    ws.on("message", (data) =>{
+        console.log(`Received Data from CLient: ${data}`);
+        ws.send(`Hello From Server, you sent ${data}`);
+    });
+
+});
